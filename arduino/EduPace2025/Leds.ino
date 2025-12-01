7eb7bd28315092fd4c0755f0abf94957eed3b782
shyynn <shnurlu02@gmail.com>
2025-12-01 14:25:33 +0100

    Update Leds.ino

--- arduino/EduPace2025/Leds.ino
+++ arduino/EduPace2025/Leds.ino
@@ -5,18 +5,21 @@ void updateLeds() {
   if (greenState) {
     lv_obj_set_style_bg_color(left_circle, lv_color_hex(0x00FF00), LV_PART_MAIN); // green visible
     lv_obj_set_style_bg_opa(left_circle, LV_OPA_COVER, LV_PART_MAIN);
-  } else {
-        lv_obj_set_style_bg_color(left_circle, lv_color_white(), LV_PART_MAIN);
+  } 
+  else {
+    lv_obj_set_style_bg_color(left_circle, lv_color_white(), LV_PART_MAIN);
+    lv_obj_set_style_bg_opa(left_circle, LV_OPA_TRANSP, LV_PART_MAIN);
   }
 
   // --- Blue LED ---
   if (blueState) {
     lv_obj_set_style_bg_color(right_circle, lv_color_hex(0x0000FF), LV_PART_MAIN); // blue visible
     lv_obj_set_style_bg_opa(right_circle, LV_OPA_COVER, LV_PART_MAIN);
-} else {
-      lv_obj_set_style_bg_color(right_circle, lv_color_white(), LV_PART_MAIN);
+  } 
+  else {
+    lv_obj_set_style_bg_color(right_circle, lv_color_white(), LV_PART_MAIN);
     lv_obj_set_style_bg_opa(right_circle, LV_OPA_TRANSP, LV_PART_MAIN);
-
+  }
 }
 
 void handleSerialSignals() {
